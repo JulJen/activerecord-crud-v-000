513383235c56740af455f8d595a4dc00a1a966f8
Jul Jen <juliajen.prog@gmail.com>
2018-03-25 21:58:48 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -86,7 +86,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  Movie.where(title: "That One Where the Guy Kicks Another Guy Once").destrou_a;;
   __
 end
 
