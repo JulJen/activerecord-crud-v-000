53e58650a228ed1bfa8ebfb8f6e360eaeb1b8da0
Jul Jen <juliajen.prog@gmail.com>
2018-03-25 21:51:37 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -73,7 +73,7 @@ def can_update_using_update_method
 
   movie = Movie.find_by(title: "Wat?")
   movie.update(title: "Wat, huh?")
-  movie.save
+  # movie.save
 end
 
 def can_update_multiple_items_at_once
