d91fac4f19d3f429f26e0c34e07a5eabdf34d9b0
Jul Jen <juliajen.prog@gmail.com>
2018-03-25 21:52:28 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,7 +79,7 @@ end
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
   5.times do |i|
-    movies = []
+    
     movies = Movie.create(title: "Movie_#{i}", release_date: 2000+i)
     # movies = Movie.find_by(:title)
     movies.update(title: "A Movie")
